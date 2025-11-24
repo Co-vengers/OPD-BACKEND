817152377eb66687ea0812e8f34beb179f9c8a57
Co-vengers <vyomrohila@gmail.com>
2025-11-24 18:35:10 +0530

    Updated database

--- app/database.py
+++ app/database.py
@@ -2,16 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# Docker will provide this env var. Fallback to sqlite for local testing if needed.
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./claims.db")
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def get_db():
